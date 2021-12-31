e6335078ed2e16f85d18adcc79da5856aa63eebd
topzz100 <adenekantope@gmail.com>
2021-12-31 17:24:53 +0100

    register user done

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const users = require('./routes/users')
 const auth = require('./routes/auth')
 
 connectDB(process.env.MONGO_URI)
+app.use(express.json())
 
 app.use(helmet())
 app.use(morgan('common'))
 
 app.use('/api/users', users)
+app.use('/api/auth', auth)
 
 const port = 5000
 app.listen(port, () => { console.log(`server listening at port ${port}`)})
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,25 @@
 const express = require('express')
-const route = express.Router()
+const router = express.Router()
+const User = require('../models/user')
+const bcrypt = require("bcrypt");
 
-route.get('/', (req, res) => {
-  res.send('this is for users')
-})
+
+//register user
+router.post("/register", async (req, res) => {
+  try {
+    //gen new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+   // create new user
+    const user = await User.create({...req.body, password: hashedPassword});
+    //save user and respond
+   
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err)
+    console.log(err)
+  }
+});
+
+module.exports = router
